c8b0805d02e9368e6231b55f44ef719e5bcf395a
ZAKI <appkari.0@gmail.com>
2024-03-27 21:02:01 +0900

    多分汎用ジェネレータになった

--- script.js
+++ script.js
@@ -1,10 +1,56 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const targetWord = "ミルクちゃん";
+    const getDefault = (value, defaultValue) => (value == null || value == "") ? defaultValue : value;
 
+    const params = new URLSearchParams(location.search);
+    const targetWord = getDefault(params.get("word"), "ミルクちゃん");
+    const  displayTitle = getDefault(params.get("title"), getDefault(params.get("word"), "ミルクちゃんチャレンジ"));
+
+    const title = document.getElementById("title");
     const button = document.getElementById("button");
+    const buttonTransition = document.getElementById("button-transition");
+    const panel = document.getElementById("transition-panel");
     const output = document.getElementById("output");
+    const inputTitle = document.getElementById("input-title");
+    const inputWord = document.getElementById("input-word");
+    const buttonLoad = document.getElementById("button-load");
     const counter = document.getElementById("counter");
     const countmsg = document.getElementById("countmsg");
+
+    const trustedOrigin = "*";
+    let loadTimer;
+
+    title.textContent = displayTitle;
+    panel.style.display = "none";
+
+    window.addEventListener("message", (ev) => {
+        if (ev.origin != trustedOrigin) return;
+        switch (ev.data.cmd) {
+            case "ack":
+                if (loadTimer != null) {
+                    clearTimeout(loadTimer);
+                    loadTimer = null;
+                }
+                break;
+            default:
+                break;
+        }
+    })
+
+    buttonTransition.addEventListener("click", () => {
+        panel.style.display = (panel.style.display == "block")
+        ? "none"
+        : "block";
+    });
+
+    buttonLoad.addEventListener("click", () => {
+        const url = new URL(location.href);
+        url.searchParams.set("title", inputTitle.value);
+        url.searchParams.set("word", inputWord.value);
+        window.parent.postMessage({cmd: "location", data: url.href}, trustedOrigin);
+        loadTimer = setTimeout(() => {
+            location.replace(url);
+        }, 10);
+    });
 
     button.addEventListener("click", () => {
         const worker = new Worker("worker.js");
